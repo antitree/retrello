679eb95e08358cef3ba60d6663d84a81f142702f
Antitree <antitree@gmail.com>
2015-07-25 18:33:24 -0400

    adding function to autoremove due dates from done cards

--- retrello.py
+++ retrello.py
@@ -18,6 +18,7 @@ DEBUG = False
 def main():
     # Check SQLite DB for records
     # select * from db where time < time+(freq)
+    clear_done()
     conn, cur = db_setup()
     results = cur.execute('SELECT name FROM sqlite_master WHERE type="table"')
     boards = results.fetchall()
@@ -30,26 +31,26 @@ def main():
         conn.close()
         # if last_time < frequency then make new card
         current = time.time()
-	print("Current time: %s" % current)
-        day = 86400
-        week = day * 7
-        month = day * 30
-        for record in records:
-            card = set_card(record)
-            if card["freq"] == "Daily" and card["last"] < (current - day):
-                card["due"] = datetime.datetime.now() + datetime.timedelta(days=1)
-                add_card(card, board)
-            elif card["freq"] == "Weekly" and card["last"] < (current - week):
-                card["due"] = datetime.datetime.now() + datetime.timedelta(days=7)
-                add_card(card, board)
-            elif card["freq"] == "Monthly" and card["last"] < (current - month):
-                card["due"] = datetime.datetime.now() + datetime.timedelta(days=30)
-                add_card(card, board)
-            elif DEBUG:
-                card["due"] = datetime.datetime.now() + datetime.timedelta(days=60)
-                add_card(card,board)
-            else:
-                print("%s: Does not need adding" % card["name"])
+    print("Current time: %s" % current)
+    day = 86400
+    week = day * 7
+    month = day * 30
+    for record in records:
+        card = set_card(record)
+        if card["freq"] == "Daily" and card["last"] < (current - day):
+            card["due"] = datetime.datetime.now() + datetime.timedelta(days=1)
+            add_card(card, board)
+        elif card["freq"] == "Weekly" and card["last"] < (current - week):
+            card["due"] = datetime.datetime.now() + datetime.timedelta(days=7)
+            add_card(card, board)
+        elif card["freq"] == "Monthly" and card["last"] < (current - month):
+            card["due"] = datetime.datetime.now() + datetime.timedelta(days=30)
+            add_card(card, board)
+        elif DEBUG:
+            card["due"] = datetime.datetime.now() + datetime.timedelta(days=60)
+            add_card(card,board)
+        else:
+            print("%s: Does not need adding" % card["name"])
     conn.close()
 
 def set_card(record):
@@ -129,6 +130,21 @@ def add_card(card, board):
 
 def delete_card(card):
     card.set_closed(True)
+
+def clear_done(done="Done"):
+    # remove due dates for all cards in done list
+    client = trello_auth()
+    #try:
+    if True:
+        tboard = next(x for x in client.list_boards())
+        tlist = next(x for x in tboard.all_lists())
+        cards = tlist.list_cards()
+        for card in cards:
+            card._set_remote_attribute("due", "null")
+        print("Removed due dates from %s" % done)
+    #except Exception as err:
+    else:
+        print("balls")
 
 def update_last(card, board):
     conn, cur = db_setup()
